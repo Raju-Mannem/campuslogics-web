c0f0681570c125b91be4647df66ae976d9f4c2da
Raju Mannem <rajumannem71@gmail.com>
2026-05-04 15:23:01 +0530

    fix: authjs header trust host

--- src/auth.ts
+++ src/auth.ts
@@ -15,6 +15,7 @@ class InvalidLoginError extends CredentialsSignin {
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Credentials({
       name: 'Credentials',
