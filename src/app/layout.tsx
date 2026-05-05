890bd861fab43a382b01dce3e0a907556cfe3326
Raju Mannem <rajumannem71@gmail.com>
2026-05-05 16:05:54 +0530

    test vars

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,8 +51,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   console.log('ENV CHECK:', {
-    AUTH_SECRET: !!process.env.AUTH_SECRET,
-    DATABASE_URL: !!process.env.DATABASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
     AUTH_URL: process.env.AUTH_URL,
   });
 
